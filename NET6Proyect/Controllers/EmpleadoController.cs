2c8f14b8055b5b75ee63d4e90ce2af1188ac317c
COls <c-cols@hotmail.com>
2023-08-17 00:10:54 -0300

    Se crea libreria Contract y Controlador EmpleadoController

---
+++ NET6Proyect/Controllers/EmpleadoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Entities.Models;
+
+namespace NET6Proyect.Controllers
+{
+    [ApiController]
+    [Route("[empleado]")]
+    public class EmpleadoController : ControllerBase
+    {        
+        public readonly DbtrabajoContext _dbContext;
+
+        public EmpleadoController(DbtrabajoContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        [HttpGet]
+        [Route("[Obtener]")]
+
+        public IActionResult GetEmpleado()
+        {
+            List<Empleado> list = new List<Empleado>(); 
+            
+            try
+            {
+                list= _dbContext.Empleados.ToList();
+
+                return StatusCode(StatusCodes.Status200OK, new {mensaje = "ok", Response = list });   
+
+            }
+            catch (Exception ex) {
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex, Response = list });
+            }
+        }
+
+
+    }
+}
--- NET6Proyect/Program.cs
+++ NET6Proyect/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Cadena de conexion
+builder.Services.AddDbContext<DbtrabajoContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL")));
 
 var app = builder.Build();
 
